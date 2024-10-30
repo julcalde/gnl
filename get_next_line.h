04b4f4bc0125338b0a2734931bddd55dc497dc4b
Julio Max Calderone Da Silva <julcalde@2-G-8.42heilbronn.de>
2024-10-30 14:00:55 +0100

    created and started necessary turn-in files

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 13:50:22 by julcalde          #+#    #+#             */
+/*   Updated: 2024/10/30 13:59:40 by julcalde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
