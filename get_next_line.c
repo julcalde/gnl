1c36352da688d4c0d1e684150c6e055960858453
Julio Max Calderone Da Silva <julcalde@1-A-30.42heilbronn.de>
2024-11-06 16:30:57 +0100

    saving progress: got a better idea for starting the project

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 13:48:58 by julcalde          #+#    #+#             */
-/*   Updated: 2024/11/03 15:28:58 by julcalde         ###   ########.fr       */
+/*   Updated: 2024/11/06 16:29:29 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/*
+/* Goal
 DESC:	Write a function that returns a line read from a
 		file descriptor.
 RETURN:	Read line if correct behavior.
@@ -22,8 +22,35 @@ NOTE:	The returned line should include the terminating \n
 		does not end with a \n character.
 NOTE:	Add all the helper functions you need in the get_next_line_utils.c file
 */
+/* Read function prototype
+int read(int fd, char buffer[], int bytes);
+fd:		file descriptor.
+buffer:	array where to stock the read chars.
+bytes:	number of bytes to read per iteration.
+*/
 
 char	*get_next_line(int fd)
 {
+	char		*text_storage;
+	static char	*read_chunk;
+	char		*tmp_buff;
+	int			i;
+
+	read_chunk = read(fd, tmp_buff, BUFFER_SIZE);
+	if (fd < 0 || BUFFER_SIZE < 1 || read_chunk < 0)
+		return (free(read_chunk), NULL);
+	text_storage = NULL;
+	ft_append_line(read_chunk, text_storage);
 	return (NULL);
+}
+
+static char	*ft_append_line(read_chunk, text_storage)
+{
+	char	*line;
+
+	line = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (line == NULL)
+		return (NULL);
+	line = (char *)read_chunk;
+	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,15 +6,64 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 13:58:22 by julcalde          #+#    #+#             */
-/*   Updated: 2024/11/03 16:14:26 by julcalde         ###   ########.fr       */
+/*   Updated: 2024/11/06 15:43:18 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/* Read function prototype
-read(int fd, char buffer[], int bytes);
-fd:		file descriptor.
-buffer:	array where to stock the read chars.
-bytes:	number of bytes to read per iteration.
+/* Utils
+Here come additional functions from libft
 */
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *) &s[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *) &s[i]);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s3;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	c1;
+	size_t	c2;
+
+	c1 = 0;
+	c2 = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	s3 = (char *) malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!s3)
+		return (NULL);
+	while (s1[c1])
+		s3[c2++] = s1[c1++];
+	c1 = 0;
+	while (s2[c1])
+		s3[c2++] = s2[c1++];
+	s3[c2] = '\0';
+	return (s3);
+}
